eea637b7ea40757ee34d9fd8797f4a70b982efb7
zhu_guang_wei <zhu_guang_wei@qq.com>
2020-11-24 11:15:01 +0800

    save

---
+++ dynamic/url_for.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from flask import Flask,redirect,url_for
+app = Flask(__name__)
+ 
+@app.route('/')
+def index():
+    return 'index'
+ 
+@app.route('/login/')
+def login():
+    return 'login'
+
+@app.route('/profile/<user>')
+def profile():
+    return 'profile'
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login')) 
+    print(url_for('login', user = 'tom'))
+    print(url_for('profile', user = 'jerry'))
+    print(url_for('static', filename = 'script.js'))
